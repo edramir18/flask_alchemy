5647959e1fb62c733ad99a0084cce85b00a3af60
Eduardo Ramirez <edramir18@gmail.com>
2020-04-25 12:15:36 +0200

    Version de prueba

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import render_template
+
+import config
+
+connex_app = config.connex_app
+
+connex_app.add_api('swagger.yml')
+
+
+@connex_app.route('/')
+def home():
+    return render_template(home.html)
+
+
+if __name__ == '__main__':
+    connex_app.run(debug=True)
